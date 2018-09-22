164a98a44fe335f848c0a0ce0676445bfe3d43b5
LilliamHaro <qualia950@gmail.com>
2018-09-21 22:04:30 -0500

    follow-m

---
+++ js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+    
+
+    var A = document.querySelector.bind(document);
+    var Aon = document.addEventListener.bind(document);
+
+    var xmouse, ymouse;
+    Aon('mousemove', function (e) {
+         xmouse = e.clientX || e.pageX;
+         ymouse = e.clientY || e.pageY;
+    });
+
+    var ball = A('#ball');
+    var x = void 0,
+         y = void 0,
+         dx = void 0,
+         dy = void 0,
+         tx = 0,
+         ty = 0,
+         key = -1;
+
+    var followMouse = function followMouse() {
+         key = requestAnimationFrame(followMouse);
+
+         if(!x || !y) {
+              x = xmouse;
+              y = ymouse;
+         } else {
+              dx = (xmouse - x) * 0.125;
+              dy = (ymouse - y) * 0.125;
+              if(Math.abs(dx) + Math.abs(dy) < 0.1) {
+                   x = xmouse;
+                   y = ymouse;
+              } else {
+                   x += dx-3.5;
+                   y += dy-3.5;
+              }
+         }
+         ball.style.left = x + 'px';
+         ball.style.top = y + 'px';
+    };
+
+    followMouse();
+
+    function hola(){
+        alert('465');
+    }
+    $('#btn').on('click',function(event){
+
+        alert(12345678889)
+    })
+
+    $('.btn2').on('click',function(event){
+
+        alert(123456788888888888888888889)
+    })
+
+    // document.getElementById("btn").addEventListener("click", hola);
+
+    
+})
