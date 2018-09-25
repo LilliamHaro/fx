97633cbda1b217b450bf6b22ab3186f50ae588f1
LilliamHaro <qualia950@gmail.com>
2018-09-25 17:15:21 -0500

    masks tests

--- js/main.js
+++ js/main.js
@@ -1,70 +1,73 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var A = document.querySelector.bind(document);
+  var Aon = document.addEventListener.bind(document);
 
-    
+  var xmouse, ymouse;
+  Aon("mousemove", function(e) {
+    xmouse = e.clientX || e.pageX;
+    ymouse = e.clientY || e.pageY;
+  });
 
-    var A = document.querySelector.bind(document);
-    var Aon = document.addEventListener.bind(document);
+  var ball = A("#ball");
+  var x = void 0,
+    y = void 0,
+    dx = void 0,
+    dy = void 0,
+    tx = 0,
+    ty = 0,
+    key = -1;
 
-    var xmouse, ymouse;
-    Aon('mousemove', function (e) {
-         xmouse = e.clientX || e.pageX;
-         ymouse = e.clientY || e.pageY;
-    });
+  var followMouse = function followMouse() {
+    key = requestAnimationFrame(followMouse);
 
-    var ball = A('#ball');
-    var x = void 0,
-         y = void 0,
-         dx = void 0,
-         dy = void 0,
-         tx = 0,
-         ty = 0,
-         key = -1;
+    if (!x || !y) {
+      x = xmouse;
+      y = ymouse;
+    } else {
+      dx = (xmouse - x) * 0.125;
+      dy = (ymouse - y) * 0.125;
+      if (Math.abs(dx) + Math.abs(dy) < 0.1) {
+        x = xmouse;
+        y = ymouse;
+      } else {
+        x += dx - 3.5;
+        y += dy - 3.5;
+      }
+    }
+    ball.style.left = x + "px";
+    ball.style.top = y + "px";
+  };
 
-    var followMouse = function followMouse() {
-         key = requestAnimationFrame(followMouse);
+  followMouse();
 
-         if(!x || !y) {
-              x = xmouse;
-              y = ymouse;
-         } else {
-              dx = (xmouse - x) * 0.125;
-              dy = (ymouse - y) * 0.125;
-              if(Math.abs(dx) + Math.abs(dy) < 0.1) {
-                   x = xmouse;
-                   y = ymouse;
-              } else {
-                   x += dx-3.5;
-                   y += dy-3.5;
-              }
-         }
-         ball.style.left = x + 'px';
-         ball.style.top = y + 'px';
-    };
+  setTimeout(function() {
+    $(".line-loader").addClass("w-100");
+  }, 0);
 
-    followMouse();
+  // setTimeout(function(){
+  //     $('.cara').addClass('mascara-1');
+  // },400)
 
-    setTimeout(function(){
-        $('.line-loader').addClass('w-100');
-    },0)
+  setTimeout(function() {
+    $(".loader-1").addClass("loader-out");
+  }, 2000);
 
-    setTimeout(function(){
-        $('.loader-1').addClass('loader-out');
-    },2000)
+  setTimeout(function() {
+    $(".loader-2").addClass("loader-out");
+  }, 2250);
+  setTimeout(function() {
+    $(".loader-3").addClass("loader-out");
+    $("main").fadeIn("slow");
+    setTimeout(function() {
+      $(".cara").addClass("mascara-1");
+    }, 400);
 
-    setTimeout(function(){
-        $('.loader-2').addClass('loader-out');
-    },2250)
-    setTimeout(function(){
-        $('.loader-3').addClass('loader-out');
-        $('main').fadeIn('slow');
-    },2500)
+    // setTimeout(function() {
+    //   $(".dos").addClass("mascara-1");
+    // }, 1800);
+  }, 2500);
 
-    setTimeout(function(){
-        $('#ball').addClass('opacity-1');
-    },6000)
-
-    
-
-   
-    
-})
+  setTimeout(function() {
+    $("#ball").addClass("opacity-1");
+  }, 6000);
+});
